729d38d98570e0d18d5662b19b2c2492a401254c
kriskrystev <krystev.kristiyan@gmail.com>
2023-12-17 16:13:44 +0200

    separate page related dtos into own files; create enum for order

---
+++ src/core/dto/page-options.dto.ts
@@ -0,0 +1,26 @@
+import { IsEnum, IsInt, IsOptional, Max, Min } from 'class-validator';
+import { Order } from '../enum/order.enum';
+import { Type } from 'class-transformer';
+
+export class PageOptionsDto {
+  @IsEnum(Order)
+  @IsOptional()
+  order?: Order = Order.ASC;
+
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @IsOptional()
+  page?: number = 1;
+
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(50)
+  @IsOptional()
+  take?: number = 10;
+
+  get skip(): number {
+    return (this.page - 1) * this.take;
+  }
+}
